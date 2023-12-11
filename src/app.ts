b8c1f310fa330637ffe9f44a2be5e01f237e8212
Anthony Pillot <pillot.anthony@gmail.com>
2023-12-11 18:42:46 +0100

    chore(xp): update console output in `Application.print()` method

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,14 @@ import os from "os";
  */
 class Application {
   static print() {
-    console.log("Hello World from TypeScript starter kit!");
-    console.log(`Node version: ${process.version}.`);
-    console.log(`Running on ${os.platform()}.`);
-    console.log(`Number of CPUs on this machine: ${os.cpus().length}.`);
+    console.log("Hello World from TypeScript starter kit!\n");
+
+    console.log(`Node version: ${process.version}. Running on ${os.type()} ${os.arch()} with hostname ${os.hostname()}.`);
+    console.log(
+      `Number of CPUs on this machine: ${os.cpus().length} ; total memory ${(os.totalmem() / 1024 / 1024 / 1024).toFixed(
+        2
+      )}GB and free memory ${(os.freemem() / 1024 / 1024 / 100).toFixed(2)}GB.`
+    );
   }
 }
 
